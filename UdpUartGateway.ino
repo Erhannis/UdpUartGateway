63146ced14e1857f31860f49990d37ac7d2a1d2a
erhannis <eyeillus@gmail.com>
2025-07-15 01:15:23 -0400

    Rejiggered to compile on ESP8266 again

--- UdpUartGateway.ino
+++ UdpUartGateway.ino
@@ -1,6 +1,7 @@
 #define ESP8266 1
 #define ESP32 2
-#define BOARD ESP32
+#define BOARD ESP8266
+#define EMIT_RUNNING 0
 
 //PERIODIC If ESP32, rename AsyncUDP.cpp to AsyncUDP.cpp0, else rename it back
 
@@ -14,8 +15,17 @@
 
 #include <HardwareSerial.h>
 HardwareSerial UartSerial(2);
-#define RXD 16
-#define TXD 17
+#define RXD 16 // RX2
+#define TXD 17 // TX2
+
+#define USB 1
+#if USB
+#define LOGGING UartSerial // Pins
+#define COMMS Serial // Usb
+#else
+#define LOGGING Serial // Usb
+#define COMMS UartSerial // Pins
+#endif
 
 #include "wifi.h"
 
@@ -30,53 +40,74 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
 
-  UartSerial.begin(921600, SERIAL_8N1, RXD, TXD);
+#if BOARD == ESP8266
+  UartSerial.begin(115200, SERIAL_8N1, SERIAL_FULL, RXD); //CHECK I'm not sure if this works, uses a hardcoded TXD, or what
+#elif BOARD == ESP32
+  UartSerial.begin(115200, SERIAL_8N1, RXD, TXD);
+#endif
 
-  Serial.printf("Connecting to %s ", ssid);
+
+  LOGGING.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    LOGGING.print(".");
   }
-  Serial.printf(" connected with %s\n", WiFi.localIP().toString());
+  LOGGING.printf(" connected with %s\n", WiFi.localIP().toString());
 
   WiFi.macAddress(mac);
   sprintf(charmac, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
   if(udp.listen(rx_port)) {
-    Serial.println("UDP listening");
+    LOGGING.println("UDP listening");
     udp.onPacket([](AsyncUDPPacket packet) {
       if (packet.localPort() != rx_port) { // Not sure this is useful
         return;
       }
       uint8_t* data = packet.data();
       int length = packet.length();
-      size_t c = UartSerial.write(data, length);
-      Serial.printf("Forwarded udp->uart [%d]->[%d] {", length, c);
+      size_t c = COMMS.write(data, length);
+      #if BOARD == ESP8266
+        COMMS.flush();
+      #elif BOARD == ESP32
+        COMMS.flush(true);
+      #endif
+      LOGGING.printf("Forwarded udp->uart [%d]->[%d] {", length, c);
       for (int j = 0; j < length; j++) {
-        Serial.printf("%02X", data[j]);
+        LOGGING.printf("%02X", data[j]);
       }
-      Serial.println("}");
+      LOGGING.println("}");
     });
   } else {
-    Serial.print("UDP not connected");
+    LOGGING.print("UDP not connected");
   }
+  const char* msg = "UDP/UART start\n";
+  size_t c = udp.broadcastTo((uint8_t*)msg, strlen(msg), tx_port);
+  LOGGING.printf("Forwarded uart->udp [%d]->[%d] @%d", strlen(msg), c, tx_port);
 }
+
+int lastPing = 0;
 
 void loop() {
   delay(1); //WARN This may cause problems, but I didn't want to emit a bunch of short udp packets
 
   int i = 0;
   uint8_t buffer[1024];
-  while (UartSerial.available() > 0) {
-    buffer[i++] = UartSerial.read();
+  while (COMMS.available() > 0) {
+    buffer[i++] = COMMS.read();
   }
   if (i > 0) {
     size_t c = udp.broadcastTo(buffer, i, tx_port);
-    Serial.printf("Forwarded uart->udp [%d]->[%d] {", i, c);
+    LOGGING.printf("Forwarded uart->udp [%d]->[%d] {", i, c);
     for (int j = 0; j < i; j++) {
-      Serial.printf("%02X", buffer[j]);
+      LOGGING.printf("%02X", buffer[j]);
     }
-    Serial.println("}");
+    LOGGING.println("}");
+  }
+
+  if (EMIT_RUNNING && millis() - lastPing > 1000) {
+    const char* msg = "UDP/UART running\n";
+    size_t c = udp.broadcastTo((uint8_t*)msg, strlen(msg), tx_port);
+    lastPing = millis();
   }
 }
